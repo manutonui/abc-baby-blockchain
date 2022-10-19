c2aeaa25ca3ec9ec888294cb4eae465b141e45df
manutonui <tktonui@outlook.com>
2022-10-20 00:36:47 +0300

    implemented signature class

--- index.py
+++ index.py
@@ -1,4 +1,20 @@
 from keypair import KeyPair
+from signature import Signature
 
-keyPair = KeyPair().genKeyPair(KeyPair())
-keyPair.printKeyPair()
+keyPair = KeyPair.genKeyPair(KeyPair())
+# keyPair.printKeyPair()
+
+privateKey = keyPair.getPrivateKey()
+
+publicKey = keyPair.getPublicKey()
+
+msg = "Whoever believes in Him shall not perish"
+
+signature = Signature.signData(privateKey, msg)
+
+# Signature.printSignature(signature)
+
+verifySignature = Signature.verifySignature(signature, msg, publicKey)
+
+# print(verifySignature)
+
