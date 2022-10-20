9424aaed129661f21a465a89aeea46cd265fd059
manutonui <tktonui@outlook.com>
2022-10-20 10:15:19 +0300

    added skeleton of functions to be implemented

---
+++ account.py
@@ -0,0 +1,30 @@
+import operation as OP
+
+class Account:
+
+    def __init__(self) -> None:
+        self.__accountID = None
+        self.__wallet = [] # list of keypairs
+        self.__balance = 0
+
+    @staticmethod
+    def genAccount(self) -> self:
+        return self
+
+    def addKeypairToWallet(keypair):
+        return None
+
+    def updateBalance(balance):
+        return None
+
+    def getBalance() -> int:
+        return None
+
+    def printBalance():
+        pass
+
+    def signData(msg, index) -> bytes:
+        return None
+
+    def creatOperatoin(recipient, amount, index) -> OP:
+        return OP
