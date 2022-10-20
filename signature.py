40b80d6e69329229c344fe1e14866c910727ab5e
manutonui <tktonui@outlook.com>
2022-10-20 10:15:52 +0300

    made methods static

--- signature.py
+++ signature.py
@@ -6,10 +6,12 @@ class Signature:
     def signData (privKey, msg):
         return rsa.sign(msg.encode(), privKey, 'SHA-1')
 
+    @staticmethod
     def verifySignature(sign, msg, pubKey):
         if rsa.verify(msg.encode(), sign, pubKey) == 'SHA-1':
             return True
         return False
 
+    @staticmethod
     def printSignature (sign):
         print("Signature value: "+str(sign))
