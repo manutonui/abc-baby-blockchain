889985fcb73daaea69aa65ee9ee45b5c0746694b
manutonui <tktonui@outlook.com>
2022-10-22 10:55:14 +0300

    implemented methods of the Account class in account.py

--- account.py
+++ account.py
@@ -1,30 +1,58 @@
-import operation as OP
+from keypair import KeyPair
+from signature import Signature as Sign
 
 class Account:
 
-    def __init__(self) -> None:
-        self.__accountID = None
+    def __init__(self):
+        # when creating a new account: generate first keypair to generate account id
+        self.__accountID = None # can be pubKey or its hash
         self.__wallet = [] # list of keypairs
         self.__balance = 0
 
     @staticmethod
-    def genAccount(self) -> self:
+    def genAccount(self):
+        # create a new account, generate a key pair and assign to the account
+        keypair = KeyPair.genKeyPair(KeyPair())
+        self.addKeypairToWallet(keypair)
+
+        # if accountID is empty, assign
+        if not self.__accountID:
+            self.assignAccountID(keypair.getPublicKey())
+            
         return self
 
-    def addKeypairToWallet(keypair):
+    def addKeypairToWallet(self, keypair):
+        # append keyPair to wallet
+        self.__wallet.append(keypair)
+
+    def updateBalance(self, balance):
+        self.__balance = balance
+
+    def getBalance(self) -> int:
+        return self.__balance
+
+    def printBalance(self):
+        print(self.__balance)
+
+    def signData(self, msg, index) -> bytes:
+        privateKey = self.__wallet[index]
+        return Sign.signData(privateKey, msg)
+
+    def createOperatoin(recipient, amount, index): # returns object of operation class
+        # recipient is an account object
+        # amount to transact
+        # index of key in the wallet
+
         return None
 
-    def updateBalance(balance):
-        return None
-
-    def getBalance() -> int:
-        return None
-
-    def printBalance():
+    def toString():
         pass
 
-    def signData(msg, index) -> bytes:
-        return None
+    def printAcc(self):
+        print("AccountID: "+str(self.__accountID)+"\nKeyPairs: "+str(self.__wallet))
 
-    def creatOperatoin(recipient, amount, index) -> OP:
-        return OP
+    def assignAccountID(self, pubKey): # hash pubKey and assign to accountID
+        hashVal = int(hash(pubKey))
+        hashOfPub = hashVal if hashVal > 0 else hashVal*-1 # to avoid negative values
+        self.__accountID = hashOfPub
+
--- index.py
+++ index.py
@@ -1,18 +1,17 @@
-from keypair import KeyPair
-from signature import Signature
+# from keypair import KeyPair
+# from signature import Signature
 
-keyPair = KeyPair.genKeyPair(KeyPair())
-# keyPair.printKeyPair()
+# keyPair = KeyPair.genKeyPair(KeyPair())
+# # keyPair.printKeyPair()
+# privateKey = keyPair.getPrivateKey()
+# publicKey = keyPair.getPublicKey()
 
-privateKey = keyPair.getPrivateKey()
-
-publicKey = keyPair.getPublicKey()
-
-msg = "Whoever believes in Him shall not perish"
-
-signature = Signature.signData(privateKey, msg)
-
+# msg = "Whoever believes in Him shall not perish"
+# signature = Signature.signData(privateKey, msg)
 # Signature.printSignature(signature) # print signature value
-
 # Signature.verifySignature(signature, msg, publicKey) # verify a signature
 
+# from account import Account as ACC
+
+# acc = ACC.genAccount(ACC())
+# acc.printAcc()
